be474ca4840a832acfec7fde46807ca6b0b6810e
Ryan Wilson <92859295+MovByte@users.noreply.github.com>
2023-02-21 23:08:02 -0500

    Automatically get prefix

--- proxyHref.js
+++ proxyHref.js
@@ -1,13 +1,15 @@
+import { prefix } from "/aero/config.js";
+
 /**
  * An alternative to href that redirects the url under a proxy if the site is blocked
  * @param {string} - The url to redirect to
  * @param {string} - The prefix where the proxy url's are handled
  */
-export default (url, prefix) => {
+export default (url, $prefix = prefix) => {
 	const img = document.createElement("img");
 
 	img.onload = () => url;
-	img.onerror = () => location.origin + prefix + url;
+	img.onerror = () => location.origin + $prefix + url;
 
 	img.src = new URL(url).origin + "/favicon.ico";
 
